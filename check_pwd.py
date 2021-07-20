63fefd2a7c4927e3d8215b3d8debcb030c2c9d4d
borng-osu <borng@oregonstate.edu>
2021-07-20 19:31:25 -0400

    Changed check_pwd so that passwords without lowercase letters return False to pass test3.

--- check_pwd.py
+++ check_pwd.py
@@ -5,15 +5,24 @@ def check_pwd(pwd):
     if type(pwd) is str:
         # Checks if pwd length is valid
         if 7 < len(pwd) < 20:
-            # Initializes valid symbols and symbol count
+            # Initializes valid symbols and symbol check
             symbols = "~`!@#$%^&*()_+-="
-            sym_check = 0
+            sym_check = False
+            # Initializes lowercase letter count
+            low_check = False
             # Iterates through pwd to check if valid characters present
             for i in range(len(pwd)):
-                if pwd[i] in symbols:
-                    sym_check += 1
-            # Returns False if no symbols are in pwd
-            if sym_check == 0:
+                if sym_check and low_check:
+                    break
+                elif pwd[i] in symbols:
+                    sym_check += True
+                elif pwd[i] in string.ascii_lowercase:
+                    low_check += True
+            # Returns False if no symbols in pwd
+            if not sym_check:
+                return False
+            # Returns False if no lowercase letters in pwd
+            if not low_check:
                 return False
             return True
     return False
