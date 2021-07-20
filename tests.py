a6444c8dca6170ac6c5a8f56a1e27291962263c2
borng-osu <borng@oregonstate.edu>
2021-07-20 19:26:55 -0400

    Wrote Test3 to check if password without lowercase letter is rejected.

--- tests.py
+++ tests.py
@@ -5,7 +5,7 @@ from check_pwd import check_pwd
 class Testcase(unittest.TestCase):
 
     def test1(self):
-        # Test if string of valid length returns True
+        # Tests if string of valid length returns True
         pwd = "abc123#$%ABC"
         self.assertEqual(check_pwd(pwd), True,
                          msg='{} returned {}'.format(pwd, check_pwd(pwd)))
@@ -13,6 +13,12 @@ class Testcase(unittest.TestCase):
     def test2(self):
         # Tests if string without symbol returns False
         pwd = "abc123ABC"
+        self.assertEqual(check_pwd(pwd), False,
+                         msg='{} returned {}'.format(pwd, check_pwd(pwd)))
+
+    def test3(self):
+        # Tests if string without lowercase letter returns False
+        pwd = "$$$123ABC"
         self.assertEqual(check_pwd(pwd), False,
                          msg='{} returned {}'.format(pwd, check_pwd(pwd)))
 
