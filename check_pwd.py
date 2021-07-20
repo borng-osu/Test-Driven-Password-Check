1c98f802215e9e163cb5f7c7eecd7e9943eb03fe
borng-osu <borng@oregonstate.edu>
2021-07-20 19:25:15 -0400

    Changed check_pwd to ensure pwd contains at least one valid symbol to pass Test2.

--- check_pwd.py
+++ check_pwd.py
@@ -5,5 +5,15 @@ def check_pwd(pwd):
     if type(pwd) is str:
         # Checks if pwd length is valid
         if 7 < len(pwd) < 20:
+            # Initializes valid symbols and symbol count
+            symbols = "~`!@#$%^&*()_+-="
+            sym_check = 0
+            # Iterates through pwd to check if valid characters present
+            for i in range(len(pwd)):
+                if pwd[i] in symbols:
+                    sym_check += 1
+            # Returns False if no symbols are in pwd
+            if sym_check == 0:
+                return False
             return True
     return False
