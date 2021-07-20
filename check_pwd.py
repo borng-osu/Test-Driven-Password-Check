127a0b385a18b56f2b2adb214343280fcecbfa5c
borng-osu <borng@oregonstate.edu>
2021-07-20 19:42:42 -0400

    Changed check_pwd to pass test5 by properly verifying pwd contains digits.

--- check_pwd.py
+++ check_pwd.py
@@ -2,6 +2,9 @@ import string
 
 
 def check_pwd(pwd):
+    """Function that takes pwd string as parameter,
+    checks whether it is a valid password,
+    and returns boolean upon verification."""
     # Checks if pwd is a string
     if type(pwd) is str:
         # Checks if pwd length is valid
@@ -9,9 +12,10 @@ def check_pwd(pwd):
             # Initializes valid symbols and symbol check
             symbols = "~`!@#$%^&*()_+-="
             sym_check = False
-            # Initializes lowercase and uppercase checks
+            # Initializes lowercase, digit, and uppercase checks
             low_check = False
             up_check = False
+            digit_check = False
             # Iterates through pwd to check if valid characters present
             for i in range(len(pwd)):
                 if sym_check and low_check and up_check:
@@ -22,8 +26,11 @@ def check_pwd(pwd):
                     low_check = True
                 elif pwd[i] in string.ascii_uppercase:
                     up_check = True
+                elif pwd[i] in string.digits:
+                    digit_check = True
             # Returns False if checks not passed
-            if not sym_check or not low_check or not up_check:
+            if not sym_check or not low_check \
+                    or not up_check or not digit_check:
                 return False
             return True
     return False
